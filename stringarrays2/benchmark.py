486f7996ef549b37705d7dd7664566690bd2da1f
Itamar Turner-Trauring <itamar@itamarst.org>
2020-12-01 11:26:25 -0500

    Pypolars, not any faster.

--- stringarrays2/benchmark.py
+++ stringarrays2/benchmark.py
@@ -5,6 +5,7 @@ import pyximport
 import numpy as np
 import numba
 from numba.typed import List as NumbaList
+from pypolars.series import Series as PolarsSeries
 
 pyximport.install(language_level=3)
 
@@ -43,8 +44,18 @@ def numba_apply(s: pd.Series, f) -> pd.Series:
     return pd.Series(result)
 
 
+def polars(in_arr):
+    series = PolarsSeries("strings", in_arr)
+    start = time.time()
+    result = series.str_replace_all("l", "").str_to_uppercase()
+    print(f"Polars actual logic: {time.time() - start}")
+    return result.to_numpy()
+
+
 measure("Pandas apply()", lambda: SERIES.apply(replace_then_upper))
-measure("Pandas apply() less getattr", lambda: SERIES.apply(replace_then_upper_less_getattr))
+measure(
+    "Pandas apply() less getattr", lambda: SERIES.apply(replace_then_upper_less_getattr)
+)
 measure(
     "Pandas tolist()",
     lambda: pd.Series(replace_then_upper(s) for s in SERIES.tolist()),
@@ -63,6 +74,8 @@ measure(
         cython_transform.transform_memoryview_replace_then_upper(SERIES.values)
     ),
 )
+measure("Polars", lambda: pd.Series(polars(SERIES.values)))
+
 # measure("Numba (first time)", lambda: numba_apply(SERIES, _numba_replace_then_upper))
 # measure("Numba (second time)", lambda: numba_apply(SERIES, _numba_replace_then_upper))
 
